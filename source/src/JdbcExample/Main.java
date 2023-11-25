2f84771694001f0669627f3a11762de10f541ca5
DESKTOP-LOL9SMI\Lenovo <dansimina10@gmail.com>
2023-11-25 13:56:04 +0200

    modificari dan

--- source/src/JdbcExample/Main.java
+++ source/src/JdbcExample/Main.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class Main {
    public static void main(String[] args) {
       try {
-          Class.forName("com.mysql.jdbc.Driver").newInstance();
+          Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
       }
       catch (Exception ex) {
           System.err.println("An Exception occured during JDBC Driver loading." + 
