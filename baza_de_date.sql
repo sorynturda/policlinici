76f2435fe520a31c9cdf856c11bca3e73029ee72
Sorin Turda <s.turda12@gmail.com>
2023-12-12 12:35:12 +0200

    baza de date creata, fara tabelul venituri

--- baza_de_date.sql
+++ baza_de_date.sql
@@ -5,39 +5,39 @@ USE policlinici;
 
 create table if not exists cont(
 	id int auto_increment primary key not null,
-	nume_utilizator varchar(20),
-    parola varchar(20)
+	nume_utilizator varchar(20) not null,
+    parola varchar(20) not null
 );
 
 create table if not exists utilizatori(
 	id int auto_increment primary key not null,
     id_cont int not null,
-    departament varchar(20),
-    adresa varchar(50),
-    cnp decimal(13, 0),
-    nume varchar(30),
-    prenume varchar(30),
-    telefon decimal(10, 0),
-    email varchar(30),
-	iban varchar(34),
-    data_angajarii date,
-    rod varchar(15)
+    departament varchar(20) not null,
+    adresa varchar(50) not null,
+    cnp varchar(13) not null,
+    nume varchar(30) not null,
+    prenume varchar(30) not null,
+    telefon varchar(10) not null,
+    email varchar(30) not null,
+	iban varchar(34) not null,
+    data_angajarii date not null,
+    rol varchar(15) not null
 );
 
 create table if not exists policlinici(
 	id int auto_increment primary key not null,
     id_program_functionare int not null,
-    adresa varchar(100),
-    denumire varchar(50)
+    adresa varchar(100) not null,
+    denumire varchar(50) not null
 );
 
 create table if not exists angajati(
 	id int auto_increment primary key not null,
     id_utilizator int not null,
     id_policlinica int not null,
-    functie varchar(20),
-    salariu_negociat decimal(8, 2),
-    numar_ore int
+    functie varchar(20) not null,
+    salariu_negociat decimal(8, 2) not null,
+    numar_ore int not null
 );
 
 create table if not exists medici(
@@ -117,10 +117,44 @@ create table if not exists programari(
     data_si_ora datetime not null
 );
 
-create table if not exists servicii_oferite;
-create table if not exists servicii_specialitati;
-create table if not exists servicii_oferite_programare;
-create table if not exists servicii_specialitate;
-create table if not exists servici_oferite_raport;
-create table if not exists servici_specialitate;
-create table if not exists orar;
+create table if not exists servicii(
+    id int auto_increment primary key not null,
+    id_specialitate int not null,
+    nume_serviciu varchar(50) not null,
+    pret decimal(10,2) not null,
+    durata time not null
+);
+
+create table if not exists orar(
+    id_angajat int not null,
+    orar_specific boolean not null default false,
+    zi_saptamana_sau_data varchar(20) not null,
+    ora_inceput time not null,
+    ora_sfarsit time not null
+);
+
+create table if not exists servicii_oferite(
+    id_policlinica int not null,
+    id_serviciu int not null
+);
+
+create table if not exists servicii_specialitati(
+    id_specialitate int not null,
+    id_serviciu int not null
+);
+
+create table if not exists servicii_oferite_programare(
+    id_programare int not null,
+    id_serviciu int not null
+);
+
+create table if not exists servici_oferite_raport(
+    id_raport int not null,
+    id_serviciu int not null,
+    investigatii varchar(500) not null
+);
+
+create table if not exists servici_specialitate_medic(
+    id_serviciu int not null,
+    id_specialitate int not null
+);
