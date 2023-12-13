7aa98800e0247aa22e953ea29187c760615ee877
Marius1608 <118887291+Marius1608@users.noreply.github.com>
2023-12-13 10:08:05 +0200

    Am scris proceduri ,scheletul lor .Trebuie testate

---
+++ Modul2_proceduri.sql
@@ -0,0 +1,87 @@
+DELIMITER //
+CREATE FUNCTION VenitTotalPoliclinici() RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE totalVenit DECIMAL(10, 2);
+    SELECT SUM(total) INTO totalVenit
+    FROM (
+        SELECT SUM(total) AS total
+        FROM programari p
+        INNER JOIN servicii_oferite_programare sop ON p.id = sop.id_programare
+        INNER JOIN servicii s ON sop.id_serviciu = s.id
+        GROUP BY p.id
+
+        UNION ALL
+
+        SELECT SUM(total) AS total
+        FROM bonuri_fiscale
+    ) AS venituri;
+    RETURN totalVenit;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION Cheltuieli() RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE totalCheltuieli DECIMAL(10, 2);
+    SELECT SUM(salariu_negociat * numar_ore) INTO totalCheltuieli
+    FROM angajati;
+    RETURN totalCheltuieli;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION ProfitLunar(luna INT, an INT) RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE venit DECIMAL(10, 2);
+    DECLARE cheltuieli DECIMAL(10, 2);
+
+    SELECT VenitTotalPoliclinici() INTO venit;
+    SELECT Cheltuieli() INTO cheltuieli;
+
+    RETURN venit - cheltuieli;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION ProfitMedicLocatieSpecialitate(idMedic INT) RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE venit DECIMAL(10, 2);
+    DECLARE cheltuieli DECIMAL(10, 2);
+    SELECT SUM(total) INTO venit
+    FROM (
+        SELECT SUM(total) AS total
+        FROM programari p
+        INNER JOIN servicii_oferite_programare sop ON p.id = sop.id_programare
+        INNER JOIN servicii s ON sop.id_serviciu = s.id
+        WHERE p.id_medic = idMedic
+        GROUP BY p.id
+        UNION ALL
+        SELECT SUM(total) AS total
+        FROM bonuri_fiscale bf
+        INNER JOIN rapoarte r ON bf.id_raport = r.id
+        WHERE r.id_medic = idMedic
+    ) AS venituri;
+
+    SELECT SUM(salariu_negociat * numar_ore) INTO cheltuieli
+    FROM angajati
+    WHERE id_utilizator = idMedic;
+
+    RETURN venit - cheltuieli;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION SalariuAngajat(idAngajat INT) RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE salariu DECIMAL(10, 2);
+    SELECT salariu_negociat * numar_ore INTO salariu
+    FROM angajati
+    WHERE id = idAngajat;
+    RETURN salariu;
+END //
+DELIMITER ;
+
---
+++ Modul3_proceduri.sql
@@ -0,0 +1,104 @@
+DELIMITER //
+CREATE PROCEDURE InserarePacient(
+    IN numePacient VARCHAR(20),
+    IN prenumePacient VARCHAR(20)
+)
+BEGIN
+    INSERT INTO pacienti (nume, prenume)
+    VALUES (numePacient, prenumePacient);
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ProgramarePacient(
+    IN idPacient INT,
+    IN idMedic INT,
+    IN dataSiOraProgramare DATETIME
+)
+BEGIN
+    INSERT INTO programari (id_pacient, id_medic, data_si_ora)
+    VALUES (idPacient, idMedic, dataSiOraProgramare);
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE SelectareSpecialitatiMedic(
+    IN idMedic INT
+)
+BEGIN
+    SELECT nume_specialitate, grad
+    FROM specialitati
+    WHERE id_medic = idMedic;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE EmitereBonFiscal(
+    IN idRaport INT,
+    IN idAngajat INT,
+    IN totalBon DECIMAL(10, 2)
+)
+BEGIN
+    INSERT INTO bonuri_fiscale (id_raport, id_angajat, total)
+    VALUES (idRaport, idAngajat, totalBon);
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ExtrageRaport(
+    IN idRaport INT
+)
+BEGIN
+    SELECT * FROM rapoarte
+    WHERE id = idRaport;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ActualizareRaport(
+    IN idRaport INT,
+    IN istoric VARCHAR(100),
+    IN simptome VARCHAR(100),
+    IN diagnostic VARCHAR(100),
+    IN recomandari VARCHAR(1000),
+    IN parafa BOOLEAN
+)
+BEGIN
+    UPDATE rapoarte
+    SET istoric = istoric, simptome = simptome, diagnostic = diagnostic, recomandari = recomandari, parafa = parafa
+    WHERE id = idRaport;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ValidareRaport(
+    IN idRaport INT
+)
+BEGIN
+    UPDATE rapoarte
+    SET parafa = TRUE
+    WHERE id = idRaport;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ExtragePacienti()
+BEGIN
+    SELECT * FROM pacienti;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE ExtrageRapoarte()
+BEGIN
+    SELECT * FROM rapoarte;
+END //
+DELIMITER ;
