4a53bfb6925087851f52298d6a147058763c92ff
Simina Dan-Marius <dansimina10@gmail.com>
2023-12-14 18:44:55 +0200

    Inceput de interfata grafica

---
+++ source/src/main/java/com/example/source/HelloApplication.java
@@ -0,0 +1,29 @@
+package com.example.source;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("scene-login-view.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
